f951490f8588fe852c3357c65eab3c73bf0e5be6
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-09 20:30:23 +0000

    Fix babel bug in dependencies, add bootstrap, react bootstrap, and react-icons dependencies, include bootstrap in index.js, create header and footer components in src/components and render them in app.js.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Footer = () => {
+  const currentDate = new Date().getFullYear();
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py-3'>
+            <p>ProShop &copy; {currentDate}</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
