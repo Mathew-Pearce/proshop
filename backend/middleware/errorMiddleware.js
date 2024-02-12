0d18bdb2f55bc728e0d9ea0836938d619a931f2c
Mathew-Pearce <matpearce1984@live.co.uk>
2024-02-12 03:00:16 +0000

    [Custom Error Handling Middleware] Add errorMiddleware.js to backend/middleware folder. in this file we create custom middleware handling. create notFounfHandler, and errorHandler in our error middleware. app.use() those functions in server.js. Use this middleware in product routes to handle errors.

---
+++ backend/middleware/errorMiddleware.js
@@ -0,0 +1,23 @@
+//Handle not found errors!
+const notFoundHandler = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+//Handle errors!
+const errorHandler = (error, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = error.message;
+
+  //check for mongoose validation error
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    message = `Resource not found`;
+    statusCode = 404;
+  }
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === " production" ? "🥞" : err.stack,
+  });
+};
+export { notFoundHandler, errorHandler };
--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,29 @@ import dotenv from "dotenv";
 import express from "express";
 import productRoutes from "./routes/productRoutes.js";
 import connectDB from "./config/db.js";
+import { notFoundHandler, errorHandler } from "./middleware/errorMiddleware.js";
 
+//Config
 dotenv.config();
 
 const port = process.env.PORT || 5000;
 const app = express();
-connectDB(); // connect to the database
 
+//Connect to the database
+connectDB();
+
+//Routes
 app.get("/", (req, res) => {
   res.send("API is up and running...");
 });
 
 app.use("/api/products", productRoutes);
 
+app.use(notFoundHandler);
+
+app.use(errorHandler);
+
+//Start the server
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
